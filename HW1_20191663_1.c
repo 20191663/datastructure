c5490cd68fa70e1b87f40c0a9a35b29692e19866
20191663 <91017333+20191663@users.noreply.github.com>
2021-09-20 20:40:16 +0900

    Update HW1_20191663_1.c
    adjust recursive function

--- HW1_20191663_1.c
+++ HW1_20191663_1.c
@@ -1,205 +1,140 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-void Insert(char*, char, int);
+#define MAX_STRING_LENGTH 1000
 
-int main(){
-	char str[1000];
-	scanf("%s",str);
-	
-	int *l, *m, *s;	//large:[], medium:{}, small:()
-	int num_l=0, num_m=0, num_s=0, i, j=0;
-	l = (int*)malloc(sizeof(int)*num_l);
-	m = (int*)malloc(sizeof(int)*num_m);
-	s = (int*)malloc(sizeof(int)*num_s);
-
-	for(i=0;str[i]!='\0';i++){
-		if(strchr("[]{}()",str[i])==NULL){
-			continue;
-		}
-		else if(str[i]=='['){
-			num_l++;
-			l = realloc(l, sizeof(int)*num_l);
-			l[num_l-1] = i;
-		}
-		else if(str[i]=='{'){
-			num_m++;
-			m = realloc(m, sizeof(int)*num_m);
-			m[num_m-1] = i;
-		}
-		else if(str[i]=='('){
-			num_s++;
-			s = realloc(s, sizeof(int)*num_s);
-			s[num_s-1] = i;
-		}
-		else if(str[i]==']'){
-			if(num_l==0){
-				Insert(str, '[', i);
-				i++;
-				j++;
-			}
-			else{
-				num_l--;
-				if(num_m>0 && num_s>0){
-					while(1){
-						if(m[num_m-1]>l[num_l] && m[num_m-1]>s[num_s-1]){
-							Insert(str, '}', m[num_m-1]+1);
-							num_m--;
-							i++;
-							j++;
-						}
-						else if(s[num_s-1]>l[num_l] && s[num_s-1]>m[num_m-1]){
-							Insert(str, ')', s[num_s-1]+1);
-							num_s--;
-							i++;
-							j++;
-						}
-						else
-							break;
-					}
-				}
-				if(num_m>0){
-					while(m[num_m-1]>l[num_l]){
-						Insert(str, '}', m[num_m-1]+1);
-						num_m--;
-						i++;
-						j++;
-					}
-				}
-				if(num_s>0){
-					while(s[num_s-1]>l[num_l]){
-						Insert(str, ')', s[num_s-1]+1);
-						num_s--;
-						i++;
-						j++;
-					}
-				}
-			}
-		}
-		else if(str[i]=='}'){
-			if(num_m==0){
-				Insert(str, '{', i);
-				i++;
-				j++;
-			}
-			else{
-				num_m--;
-				if(num_l>0 && num_s>0){
-					while(1){
-						if(l[num_l-1]>m[num_m] && l[num_l-1]>s[num_s-1]){
-							Insert(str, ']', l[num_l-1]+1);
-							num_l--;
-							i++;
-							j++;
-						}
-						else if(s[num_s-1]>m[num_m] && s[num_s-1]>l[num_l-1]){
-							Insert(str, ')', s[num_s-1]+1);
-							num_s--;
-							i++;
-							j++;
-						}
-						else
-							break;
-					}
-				}
-				if(num_l>0){
-					while(l[num_l-1]>m[num_m]){
-						Insert(str, ']', l[num_l-1]+1);
-						num_l--;
-						i++;
-						j++;
-					}
-				}
-				if(num_s>0){
-					while(s[num_s-1]>m[num_m]){
-						Insert(str, ')', s[num_s-1]+1);
-						num_s--;
-						i++;
-						j++;
-					}
-				}
-			}
-		}
-		else if(str[i]==')'){
-			if(num_s==0){
-				Insert(str, '(', i);
-				i++;
-				j++;
-			}
-			else{
-				num_s--;
-				if(num_l>0 && num_m>0){
-                    while(1){
-                        if(l[num_l-1]>s[num_s] && l[num_l-1]>m[num_m-1]){
-                            Insert(str, ']', l[num_l-1]+1);
-                            num_l--;
-                            i++;
-                            j++;
-                        }
-                        else if(m[num_m-1]>s[num_s] && m[num_m-1]>l[num_l-1]){
-                            Insert(str, '}', m[num_m-1]+1);
-                            num_m--;
-                            i++;
-                            j++;
-                        }
-                        else
-                            break;
-                    }
-                }			
-				if(num_m>0){
-					while(m[num_m-1]>s[num_s]){
-						Insert(str, '}', m[num_m-1]+1);
-						num_m--;
-						i++;
-						j++;
-					}
-				}
-				if(num_l>0){
-					while(l[num_l-1]>s[num_s]){
-						Insert(str, ']', l[num_l-1]+1);
-						num_l--;
-						i++;
-						j++;
-					}
-				}
-			}
-		}
-	}
- 	while(num_l>0){
-		num_l--;
-		Insert(str, ']', l[num_l]+1);
-		j++;
-	}
-	while(num_m>0){
-		num_m--;
-		Insert(str, '}', m[num_m]+1);
-		j++;
-	}
-	while(num_s>0){
-		num_s--;
-		Insert(str, ')', s[num_s]+1);
-		j++;
-	}
-	if(j==0){
-		printf("well formed string\n");
-	}
-	else{
-		printf("not well formed string\n추가되는 괄호 수: %d\n%s\n",j,str);
-	}
-
-	free(l);
-	free(m);
-	free(s);
-
-	return 0;
+// check c if it's Open Parenthesis (, {, [
+int IsOpenParen(char c)
+{
+	if (c == '(' || c == '{' || c == '[')
+		return 1;
+	else
+		return 0;
 }
 
-void Insert(char* str, char c, int i){
-	int j, loc = strlen(str);
-	str[loc+1]='\0';
-	for(j=loc;j>i;j--){
-		str[j]=str[j-1];
+// check c if it's Close Parenthesis ), }, ]
+int IsCloseParen(char c)
+{
+	if (c == ')' || c == '}' || c == ']')
+		return 1;
+	else
+		return 0;
+}
+
+// add char into newstr
+void AddChar(char* newstr, char c)
+{
+	int len = strlen(newstr);
+	newstr[len] = c;
+}
+
+// get Open Parenthesis from matched Close Parenthesis(paren)
+char GetMatchingOpenParen(char paren)
+{
+	switch (paren)
+	{
+	case ')':
+		return '(';
+	case '}':
+		return '{';
+	case ']':
+		return '[';
 	}
-	str[j] = c;
+}
+
+// get Close Parenthesis from matched Open Parenthesis(paren)
+char GetMatchingCloseParen(char paren)
+{
+	switch (paren)
+	{
+	case '(':
+		return ')';
+	case '{':
+		return '}';
+	case '[':
+		return ']';
+	}
+}
+
+// insert parenthesis into well performed paranthesis.
+void InsertParen(char* str, char* newstr, int index)
+{
+	if (IsCloseParen(str[index]))
+		AddChar(newstr, GetMatchingOpenParen(str[index]));
+	AddChar(newstr, str[index]);
+	if (IsOpenParen(str[index]))
+		AddChar(newstr, GetMatchingCloseParen(str[index]));
+}
+
+void Max(int* pair, int* first, int* last) {
+	if (pair[1] - pair[0] > *last - *first) {
+		*last = pair[1];
+		*first = pair[0];
+	}
+}
+
+// find biggest pair of parenthesis form start to end, save location into first and last.
+void BigPair(char* str, int start, int end, int* first, int* last)
+{
+	int l[2], m[2], s[2], b[2];	//large, mideum, small, big_pair [] {} ()
+	l[0] = l[1] = m[0] = m[1] = s[0] = s[1] = -1;
+	for (int i = start; i <= end; i++) {
+		switch (str[i]) {
+		case '[': if (l[0] == -1) { l[0] = i; } break;
+		case '{': if (m[0] == -1) { m[0] = i; } break;
+		case '(': if (s[0] == -1) { s[0] = i; } break;
+		case ']': if (l[0] != -1) { l[1] = i; } break;
+		case '}': if (m[0] != -1) { m[1] = i; } break;
+		case ')': if (s[0] != -1) { s[1] = i; }
+		default: continue;
+		}
+	}
+
+	Max(l, first, last);
+	Max(m, first, last);
+	Max(s, first, last);
+}
+
+void FindPair(char* str, char* newstr, int start, int end)
+{
+	if (start > end)
+		return;
+	int first = -1, last = -1;
+	BigPair(str, start, end, &first, &last);
+	if (last == -1)	//pair isn't exists where start to end.
+	{
+		for (int i = start; i <= end; i++)
+			InsertParen(str, newstr, i);
+	}
+	else
+	{
+		FindPair(str, newstr, start, first - 1);
+		AddChar(newstr, str[first]);
+		FindPair(str, newstr, first + 1, last - 1);
+		AddChar(newstr, str[last]);
+		FindPair(str, newstr, last+1, end);
+	}
+}
+
+int main()
+{
+	char str[MAX_STRING_LENGTH] = { 0 };			//input string
+	char openParens[MAX_STRING_LENGTH / 2] = { 0 };	//get open parenthesis only
+	char newstr[MAX_STRING_LENGTH * 2] = { 0 };		//output string
+	int numOP = 0;			//checking number of open parenthesis
+	int index = 0;			//checking input string index
+
+	//get string and save length
+	scanf("%s", str);
+	int startLength = strlen(str);
+
+	//recursive function to fix string from start to end.
+	FindPair(str, newstr, 0, strlen(str)-1);
+
+	//print whether well formed or not(if not, also print fixed string)
+	if ((int)strlen(newstr) == startLength)
+		printf("well formed string\n");
+	else
+		printf("not well formed string\n추가되는 괄호 수 : %d\n%s\n", (int)strlen(newstr) - startLength, newstr);
+	return 0;
 }
